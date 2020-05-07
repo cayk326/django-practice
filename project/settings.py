91e95097b76fe35520f9e85d4379fcd40c3090c4
yuta <yuta@localhost.localdomain>
2020-05-07 11:37:47 +0900

    Complete practice

--- project/settings.py
+++ project/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 LOGIN_URL = 'admin:login'
 LOGOUT_REDIRECT_URL = '/'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 
 
